84c4e02476488bac6298b1b1797fecbb1d39156c
Michiel de Jong <michiel@unhosted.org>
2022-02-02 16:10:43 +0100

    generateImpliedPurchases.php

--- generateImpliedPurchases.php
+++ generateImpliedPurchases.php
@@ -5,16 +5,27 @@ require_once("./utils.php");
 $journal = readJournal($argv[1]);
 $suppliers = readSuppliers($argv[2]);
 foreach ($journal["entries"] as $entry) {
+    $matched = false;
     foreach ($suppliers as $match => $budget) {
       $pos = strpos($entry["comment"], $match);
-        if ($pos) {
-            printTransaction([
-              "date" => $entry["date"],
-              "comment" => "Implied transaction (" . $match . ")",
-              "account1" => $entry["account2"],
-              "account2" => $budget,
-              "amount" => $entry["amount"],
-            ]);
-        }
+      if ($pos === false) {
+        $pos = strpos($entry["account2"], $match);
+      }
+      if ($pos !== false) {
+          printTransaction([
+            "date" => $entry["date"],
+            "comment" => "Implied transaction (" . $match . ")",
+            "account1" => $entry["account2"],
+            "account2" => $budget,
+            "amount" => $entry["amount"],
+          ]);
+          $matched = true;
+          break;
+      }
+    }
+    if (!$matched) {
+      echo "Could not match this one!\n";
+      var_dump($entry);
+      exit();
     }
 }
