c81f233f6c64e9702b2bcf24b70e8a2a4ce7d5ff
Michiel de Jong <michiel@unhosted.org>
2022-03-15 16:14:41 +0100

    verify

--- src/index.php
+++ src/index.php
@@ -11,9 +11,12 @@
   echo "hello world";
   $conn  = pg_connect($_ENV["DATABASE_URL"]);
   $username = pg_escape_string($_SERVER['PHP_AUTH_USER']);
-  $pwhash = pg_escape_string(password_hash($_SERVER['PHP_AUTH_PW'], PASSWORD_BCRYPT, [ 'cost' => 10 ]));
   echo $username;
-  echo $pwhash;
+  
+  $passwordGiven = $_SERVER['PHP_AUTH_PW'];
+
+  // $pwhash = pg_escape_string(password_hash($_SERVER['PHP_AUTH_PW'], PASSWORD_BCRYPT, [ 'cost' => 10 ]));
+  // echo $pwhash;
   $query = "SELECT pwhashbcryptcost10 FROM users WHERE username = '${username}'";
   echo $query;
   $result = pg_query($conn, $query);
@@ -21,6 +24,11 @@
   var_dump($result);
   echo pg_num_rows($result);
   $arr = pg_fetch_array($result, 0, PGSQL_NUM);
+  $hash = $arr[0];
+  var_dump($passwordGiven);
+  $conclusion = password_verify($passwordGiven, $hash);
   echo "array:";
   var_dump($arr);
+  echo "conclusion:";
+  var_dump($conclusion);
 ?>
