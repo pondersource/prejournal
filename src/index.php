3abd58d3f7a893ebf1eec1d162efe3aabc07c57d
Michiel de Jong <michiel@unhosted.org>
2022-03-15 16:09:50 +0100

    select for verify

--- src/index.php
+++ src/index.php
@@ -14,7 +14,7 @@
   $pwhash = pg_escape_string(password_hash($_SERVER['PHP_AUTH_PW'], PASSWORD_BCRYPT, [ 'cost' => 10 ]));
   echo $username;
   echo $pwhash;
-  $query = "SELECT * FROM users WHERE username = '${username}' AND pwhashbcryptcost10 = '${pwhash}'";
+  $query = "SELECT pwhashbcryptocost10 FROM users WHERE username = '${username}'";
   echo $query;
   $result = pg_query($conn, $query);
   echo "result:";
