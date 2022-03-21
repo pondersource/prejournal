df43913b303a6c33bd3a3082ee7fae18397ab49c
Michiel de Jong <michiel@unhosted.org>
2022-03-21 16:04:34 +0100

    schema.sql

--- schema.sql
+++ schema.sql
@@ -21,19 +21,19 @@ create table movements (
   amount float
 );
 
-create table statements {
+create table statements (
   id serial primary key,
   movementId int,
   userId int,
   sourceDocumentFormat varchar, /* could be an invoice, bank statement csv file, API call etc */
   sourceDocumentFilename varchar, /* TODO: work out how to store files when on Heroku */
   timestamp_ timestamp
-}
+)
 
-create table componentGrants {
+create table componentGrants (
   id serial primary key,
   fromUser int,
   toUser int,
   componentId int
-}
+)
 
