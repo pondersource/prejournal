09bf0f26a55b2a6e8a2d6a6b14924eb2a21f407a
Michiel de Jong <michiel@unhosted.org>
2022-03-21 12:30:10 +0100

    New tables ref #10

--- schema.sql
+++ schema.sql
@@ -20,3 +20,17 @@ create table movements (
   amount float,
   agreementId int /* ties e.g. an invoice to a payment */
 );
+
+create table statements {
+  id serial primary key,
+  movementId int,
+  userId int,
+  timestamp_ timestamp
+}
+
+create table componentGrants {
+  id serial primary key,
+  fromUser int,
+  toUser int,
+  componentId int
+}
