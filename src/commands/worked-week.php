be9fd069f135754ceb93510239cd15c654758ed4
Triantafullenia-Doumani <triantafulleniad@gmail.com>
2022-04-21 08:24:57 +0200

    implement worked_week

--- src/commands/worked-week.php
+++ src/commands/worked-week.php
@@ -6,6 +6,27 @@
 function workedWeek($context, $command) {
   if (isset($context["user"])) {
   
+    $timestamp = strtotime($command[1]);
+    $worker = $_SERVER['PREJOURNAL_USERNAM'];
+    $project = $command[2].':'.$command[3];
+    $type = 'worked';
+    $worked_hours = '40';
+    
+  /* Create Movement */
+  $movementId = intval(createMovement($context, [
+    "create-movement",
+    $type[0],
+    strval(getComponentId($worker)),
+    strval(getComponentId($project)),
+    $timestamp,
+    $worked_hours
+  ])[0]);
+  $statementId = intval(createStatement($context, [
+    "create-statement",
+    $movementId,
+    $timestamp
+  ])[0]);
+
   } else {
     return ["User not found or wrong password"];
   }
