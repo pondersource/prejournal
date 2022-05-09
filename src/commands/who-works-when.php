6d6d1974088b9190c4e8bb98c1f5d613616de40e
Michiel de Jong <michiel@unhosted.org>
2022-05-09 13:52:11 +0200

    format output of who-works-when

--- src/commands/who-works-when.php
+++ src/commands/who-works-when.php
@@ -1,19 +1,36 @@
 <?php declare(strict_types=1);
   require_once(__DIR__ . '/../platform.php');
 
+function formatDate($date) {
+  $parts = explode(" ", $date);
+  return $parts[0];
+}
 // Notice this circumvenes the statements and component grants
 // and therefore it only works in adminParty mode:
 function whoWorksWhen($context) {
   if ($context['adminParty']) {
-    $ret = [];
+    $days = [];
     $movements = getAllMovements();
     $componentNames = getAllComponentNames();
     for ($i = 0; $i < count($movements); $i++) {
       if ($movements[$i]["type_"] == 'worked') {
         $from = $componentNames[$movements[$i]["fromcomponent"]];
         $to = $componentNames[$movements[$i]["tocomponent"]];
+        $amount = $movements[$i]["amount"];
         $date = $movements[$i]["timestamp_"];
-        var_dump([$from, $to, $date]);
+        if (!isset($days[$date])) {
+          $days[$date] = [];
+        }
+        array_push($days[$date], "$from $to $amount");
+      }
+    }
+    ksort($days);
+    $ret = [];
+    foreach ($days as $date => $arr) {
+      array_push($ret, "");
+      array_push($ret, formatDate($date));
+      foreach ($days[$date] as $val) {
+          array_push($ret, $val);
       }
     }
     // var_dump($componentNames);
