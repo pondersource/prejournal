8950bc68cca044f91f6c8755125696e487d59869
Michiel de Jong <michiel@unhosted.org>
2022-05-13 15:30:13 +0200

    startDate and endDate in what-the-world-owes

--- src/commands/what-the-world-owes.php
+++ src/commands/what-the-world-owes.php
@@ -7,28 +7,65 @@ function toDate($str) {
   return $parts[0];
 }
 
+// E.g. isBefore("2022-04-24", "2023-01-01") -> true
+function isBefore($d1, $d2) {
+  $parts1 = explode('-', $d1);
+  $parts2 = explode('-', $d2);
+  if (count($parts1) != 3) {
+    throw new Error("First date is not in 1970-01-01 format: '$d1'");
+  }
+  if (count($parts2) != 3) {
+    throw new Error("Second date is not in 1970-01-01 format: '$d2'");
+  }
+  for ($i = 0; $i < 3; $i++) {
+    if (intval($parts1[$i]) < intval($parts2[$i])) {
+      return true;
+    }
+    if (intval($parts1[$i]) > intval($parts2[$i])) {
+      return false;
+    }
+  }
+  return false;
+}
+
+// start and end date are optional, so:
+// what-the-world-owes michiel
+// what-the-world-owes michiel 2022-01-01
+// what-the-world-owes michiel 2022-01-01 2023-01-01
 function whatTheWorldOwes($context, $command) {
   if ($context['adminParty']) {
     $componentId = getComponentId($command[1]);
+    if (count($command) >= 3) {
+      $startDate = $command[2];
+    } else {
+      $startDate = "1970-01-01";
+    }
+    if (count($command) >= 4) {
+      $endDate = $command[3];
+    } else {
+      $endDate = "2070-01-01";
+    }
     $movements = getAllMovements();
     // similar to the code in the who-works-when command:
     $days = [];
     $cumm = 0;
     for ($i = 0; $i < count($movements); $i++) {
-      $delta = 0;
-      if ($movements[$i]["fromcomponent"] == $componentId) {
-        $delta = -floatval($movements[$i]["amount"]);
-      }
-      if ($movements[$i]["tocomponent"] == $componentId) {
-        $delta = floatval($movements[$i]["amount"]);
-      }
-      if ($delta != 0) {
-        $date = toDate($movements[$i]["timestamp_"]);
-        if (!isset($days[$date])) {
-          $days[$date] = [];
+      $date = toDate($movements[$i]["timestamp_"]);
+      if (isBefore($startDate, $date) && isBefore($date, $endDate)) {
+        $delta = 0;
+        if ($movements[$i]["fromcomponent"] == $componentId) {
+          $delta = -floatval($movements[$i]["amount"]);
         }
-        $id = $movements[$i]["id"];
-        array_push($days[$date], $delta);
+        if ($movements[$i]["tocomponent"] == $componentId) {
+          $delta = floatval($movements[$i]["amount"]);
+        }
+        if ($delta != 0) {
+          if (!isset($days[$date])) {
+            $days[$date] = [];
+          }
+          $id = $movements[$i]["id"];
+          array_push($days[$date], $delta);
+        }
       }
     }
     ksort($days);
