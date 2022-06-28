a7b5c595803f8fd53d5a220356df8216380c2c64
Michiel de Jong <michiel@unhosted.org>
2022-06-28 15:45:14 +0200

    Move require of loadenv from database.php to cli-[batch|single].php, avoid it for server.php, fix #94

--- src/database.php
+++ src/database.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../schema.php');
-require_once(__DIR__ . '/../loadenv.php');
 
 use Doctrine\DBAL\DriverManager;
 
