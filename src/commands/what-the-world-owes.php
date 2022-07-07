0bcd2bd6284002773dcfb2dd65ad7ad7192285fc
Michiel de Jong <michiel@unhosted.org>
2022-07-07 14:20:32 +0200

    Omit triangles and squares in what-the-world-owes

--- src/commands/what-the-world-owes.php
+++ src/commands/what-the-world-owes.php
@@ -32,6 +32,32 @@ function isBefore($d1, $d2)
     return false;
 }
 
+function getCycles($componentId, $cycleLength) {
+    if ($cycleLength < 2) {
+        throw new Error("cycleLength should be at least 2");
+    }
+    $part1 = 'SELECT m1.amount AS amount, m1.id AS id1, m1.fromcomponent AS fc1';
+    $part2 = ' FROM movements m1 ';
+    $part3 = "WHERE m1.fromcomponent = :pivotId AND m$cycleLength.tocomponent = :pivotId";
+    for ($i = 2; $i <= $cycleLength; $i++) {
+        $part1 .= ", m$i.id AS id$i, m$i.fromcomponent AS fc$i";
+        $part2 .= "INNER JOIN movements m$i ON m" . ($i-1) . ".tocomponent = m$i.fromcomponent ";
+        $part3 .= " AND m" . ($i-1) . ".amount = m$i.amount AND m" . ($i-1) . ".timestamp_ = m$i.timestamp_";
+    }
+    $query = $part1 . $part2 . $part3;
+    // echo $query;
+    $conn = getDbConn();
+    $res = $conn->executeQuery($query, [ 'pivotId' => $componentId ]);
+    $ass = $res->fetchAllAssociative();
+    $res = [];
+    for ($i = 0; $i < count($ass); $i++) {
+        for ($j = 1; $j <= $cycleLength; $j++) {
+            array_push($res, $ass[$i]["id$j"]);
+        }
+    }
+    return $res;
+}
+
 // start and end date and start balance are optional, so:
 // what-the-world-owes michiel
 // what-the-world-owes michiel 2022-01-01
@@ -39,6 +65,10 @@ function isBefore($d1, $d2)
 // what-the-world-owes michiel 2022-01-01 2023-01-01 1000.0
 function whatTheWorldOwes($context, $command)
 {
+    $triangleMembers = getCycles(getComponentId($context["user"]["username"]), 3);
+    $squareMembers = getCycles(getComponentId($context["user"]["username"]), 4);
+    $cycleMembers = array_merge($triangleMembers, $squareMembers);
+
     if ($context['adminParty']) {
         $componentId = getComponentId($command[1]);
         if (count($command) >= 3) {
@@ -61,6 +91,10 @@ function whatTheWorldOwes($context, $command)
         // similar to the code in the who-works-when command:
         $days = [];
         for ($i = 0; $i < count($movements); $i++) {
+            if (in_array($movements[$i]["id"], $cycleMembers)) {
+                // echo "Skipping cycle member movement " . $movements[$i]["id"] . "\n";
+                continue;
+            }
             $date = toDate($movements[$i]["timestamp_"]);
             if (isBefore($startDate, $date) && isBefore($date, $endDate)) {
                 $delta = 0;
