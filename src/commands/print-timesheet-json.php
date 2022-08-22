36fc018d46b666f6edabd8b96c58ff1975ce629d
Michiel de Jong <michiel@unhosted.org>
2022-08-22 15:08:33 +0200

    PREJOURNAL_OPEN_MODE, fix #133

--- src/commands/print-timesheet-json.php
+++ src/commands/print-timesheet-json.php
@@ -7,18 +7,25 @@ require_once(__DIR__ . '/helpers/dataAccessObject.php');
 */
 function printTimesheetJson($context, $command)
 {
-    if (isset($context["user"])) {
+    var_dump($context);
+    if (isset($context["openMode"]) && $context["openMode"] == "true") {
+        var_dump($command);
         $project_name = $command[1];
-        $min_id = intval($command[2]);
-        $max_id = intval($command[3]);
+        $min_id = 0;
+        if (count($command) > 2) {
+            $min_id = intval($command[2]);
+        }
+        $max_id = 1000000;
+        if (count($command) > 3) {
+            $max_id = intval($command[3]);
+        }
 
-
-        $jsondata = getFromMovementAndSync($context["user"]["id"], $project_name, $min_id, $max_id);
+        $jsondata = getFromMovementAndSync($project_name, $min_id, $max_id);
 
         $result = json_encode($jsondata, JSON_PRETTY_PRINT);
 
         echo $result;
     } else {
-        return ["User not found or wrong password"];
+        return ["This command only works if the server is running in open mode! See https://github.com/pondersource/prejournal/issues/133"];
     }
 }
