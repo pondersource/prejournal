a461016c30917cd52a9b10d63ea4896d3b04f3ca
Michiel de Jong <michiel@unhosted.org>
2022-08-22 16:55:51 +0200

    new db table accessControl

--- schema.php
+++ schema.php
@@ -7,52 +7,59 @@ function getTables()
     return [
         "drop table if exists users;",
         "create table users (
-          id SERIAL PRIMARY KEY,
-          /**uuid uuid DEFAULT uuid_generate_v4 (),**/
-          username varchar(54) UNIQUE,
-          passwordhash varchar
-        );",
+  id SERIAL PRIMARY KEY,
+  /**uuid uuid DEFAULT uuid_generate_v4 (),**/
+  username varchar(54) UNIQUE,
+  passwordhash varchar
+);",
 
         "drop table if exists components;",
         "create table components (
-          id SERIAL PRIMARY KEY,
-          name varchar,
-          UNIQUE(name)
-        );",
+  id SERIAL PRIMARY KEY,
+  name varchar,
+  UNIQUE(name)
+);",
 
         "drop table if exists movements;",
         "create table movements (
-          id SERIAL PRIMARY KEY,
-          userId integer,
-          type_ varchar(54), /* 'invoice', 'payment', 'worked' */
-          fromComponent integer,
-          toComponent integer,
-          timestamp_ timestamp,
-          amount decimal
-        );",
+  id SERIAL PRIMARY KEY,
+  userId integer,
+  type_ varchar(54), /* 'invoice', 'payment', 'worked' */
+  fromComponent integer,
+  toComponent integer,
+  timestamp_ timestamp,
+  amount decimal
+);",
 
         "drop table if exists statements;",
         "create table statements (
-          id SERIAL PRIMARY KEY,
-          movementId integer,
-          userId integer,
-          sourceDocumentFormat varchar, /* could be an invoice, bank statement csv file, API call etc */
-          sourceDocumentFilename varchar, /* TODO: work out how to store files when on Heroku */
-          timestamp_ timestamp,
-          description varchar,
-          internal_type varchar,
-          remote_id  varchar,
-          UNIQUE(remote_id),
-          remote_system varchar
-        );",
+  id SERIAL PRIMARY KEY,
+  movementId integer,
+  userId integer,
+  sourceDocumentFormat varchar, /* could be an invoice, bank statement csv file, API call etc */
+  sourceDocumentFilename varchar, /* TODO: work out how to store files when on Heroku */
+  timestamp_ timestamp,
+  description varchar,
+  internal_type varchar,
+  remote_id  varchar,
+  UNIQUE(remote_id),
+  remote_system varchar
+);",
 
         "drop table if exists componentGrants;",
         "create table componentGrants (
-          id SERIAL PRIMARY KEY,
-          fromUser numeric,
-          toUser numeric,
-          componentId numeric
-        );",
+  id SERIAL PRIMARY KEY,
+  fromUser numeric,
+  toUser numeric,
+  componentId numeric
+);",
+
+        "drop table if exists accessControl;",
+        "create table accessControl (
+  id SERIAL PRIMARY KEY,
+  componentId numeric,
+  userId numeric
+);",
     ];
 }
 
