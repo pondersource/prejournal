b94c970f1f75bc7ae0db2a2155912b7193a9f7cf
Michiel de Jong <michiel@unhosted.org>
2022-08-24 11:33:28 +0200

    oops

--- src/database.php
+++ src/database.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../schema.php');
 
-use Doctrine\DBAM\DriverManager;
+use Doctrine\DBAL\DriverManager;
 
 function getDbConn()
 {
