b3ff568a0100f0b3e0f4fbe50d09e6b3020eb77a
Michiel de Jong <michiel@unhosted.org>
2022-08-26 16:36:37 +0200

    Allow specifying worker component name, fix #143

--- src/commands/worked-hours.php
+++ src/commands/worked-hours.php
@@ -5,15 +5,30 @@ require_once(__DIR__ . '/../platform.php');
 require_once(__DIR__ . '/helpers/createMovement.php');
 require_once(__DIR__ . '/helpers/createStatement.php');
 
+//                                                 date              client       project             numHours description worker    
 // E.g.: php src/cli-single.php  worked-hours "20 September 2021" "stichting" "Peppol for the Masses" 4
-// E.g.: php src/cli-single.php  worked-hours "20 September 2021" "stichting" "Peppol for the Masses" 4 "Last hours"
+// E.g.: php src/cli-single.php  worked-hours "20 September 2021" "stichting" "Peppol for the Masses" 4        "Last hours"
+// E.g.: php src/cli-single.php  worked-hours "20 September 2021" "stichting" "Peppol for the Masses" 4        "Last hours" george
 
 function workedHours($context, $command)
 {
     if (isset($context["user"])) {
         $conn  = getDbConn();
         $timestamp = strtotime($command[1]);
-        $worker = $context["user"]["username"];
+        // Note that you cannot just enter any worker's name here,
+        // you will still have to go through the `hasAccess` check
+        // which checks whether the authenticated user has claimed
+        // the worker component.
+        // In the current setup, claiming components is first-come-first-server
+        // For instance, if the user `m-ld` claims components `angus` and `george`,
+        // it means that from then on, only user `m-ld` will be able to edit
+        // the timesheets of Angus and George.
+        // Tip: if a user is only used for one user, make the user name and the
+        // worker component name match, then you can leave off the 7th argument
+        // and it will just default to that.
+        // For instance user `michiel`
+        // can claim the component of worker `michiel`.
+        $worker = (count($command) >= 7 ? $command[6] : $context["user"]["username"]);
         $project = $command[2].':'.$command[3];
         $type = 'worked';
         $worked_hours = $command[4];
