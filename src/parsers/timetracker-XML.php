f37f329981bd2e6a578d0716ca92656fe7e32a9c
Michiel de Jong <michiel@unhosted.org>
2022-08-29 15:28:53 +0200

    test fixes

--- src/parsers/timetracker-XML.php
+++ src/parsers/timetracker-XML.php
@@ -9,13 +9,14 @@ function parseTimeTrackerXML($str)
     $xml = simplexml_load_string($str, 'SimpleXMLElement', LIBXML_NOCDATA);
     $xmlJson = json_encode($xml);
     $xmlArr = json_decode($xmlJson); // Returns associative array
-    //var_dump($xmlArr);
-
+    // var_dump($xmlArr);
+    $start = strtotime($xmlArr->row->date . " " . $xmlArr->row->start);
+    $finish = strtotime($xmlArr->row->date . " " . $xmlArr->row->finish);
     array_push($ret, [
         "worker" => $xmlArr->row->user,
         "project" => "test",
-        "start" => strtotime($xmlArr->row->date),
-        "seconds" => $xmlArr->row->finish,
+        "start" => $start,
+        "seconds" => $finish - $start,
     ]);
     return $ret;
 }
