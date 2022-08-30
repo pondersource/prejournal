78342461822a013b4eb9c17567a7a464f123a73f
Michiel de Jong <michiel@unhosted.org>
2022-08-30 09:33:38 +0200

    update pta-me

--- src/commands/pta-me.php
+++ src/commands/pta-me.php
@@ -5,12 +5,17 @@ require_once(__DIR__ . '/../database.php');
 function ptaMe($context)
 {
     if (isset($context["user"])) {
-        $movements = getMovementsForUser($context["user"]["id"]);
+        $conn = getDbConn();
+        $consumptionRes = $conn->executeQuery('SELECT ' .
+            'm.timestamp_, m.amount, m.unit, s.description ' .
+            'FROM movements m INNER JOIN implications i ON i.movementId = m.id INNER JOIN statements s ON i.statementId = s.id ' .
+            'WHERE i.relation=\'delivery\' AND m.tocomponent = 1');
+        $movements = $consumptionRes->fetchAllAssociative();
         $ret = [];
         for ($i = 0; $i < count($movements); $i++) {
-            array_push($ret, $movements[$i]["timestamp_"]);
-            array_push($ret, "assets  " . $movements[$i]["amount"]);
-            array_push($ret, "income");
+            array_push($ret, $movements[$i]["timestamp_"] . "  " . $movements[$i]["description"]);
+            array_push($ret, "expenses  " . $movements[$i]["amount"]);
+            array_push($ret, "bank");
             array_push($ret, "");
         }
         return $ret;
