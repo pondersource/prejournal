5bb36d3abfe24fc1898fa0dbf3e2fdb1d5f7166c
Michiel de Jong <michiel@unhosted.org>
2023-03-19 09:00:28 +0100

    Timesheet class

---
+++ src/pj2-based/Timesheet.php
@@ -0,0 +1,141 @@
+<?php
+
+require_once(__DIR__ . '/../utils.php');
+
+const ANALYSIS_FIRST_WEEK = "202101";
+const ANALYSIS_LAST_WEEK = "202310";
+const DEFAULT_HOURS_PER_WEEK = 40;
+
+class Timesheet {
+  private $structured = [];
+  function __construct() {
+
+  }
+  function addEntries($newEntries) {
+    for ($i = 0; $i < count($newEntries); $i++) {
+      if ($newEntries[$i]["type"] == "worked") {
+        $organization = $newEntries[$i]["organization"];
+        $worker = $newEntries[$i]["worker"];
+        $week = dateTimeToWeekOfYear($newEntries[$i]["date"]);
+        $this->ensureDataStructure($organization, $worker, $week);
+        $this->structured[$organization][$worker][$week]["hoursWorked"] += $newEntries[$i]["hours"];
+        $this->structured[$organization][$worker][$week]["details"] .= "" . $newEntries[$i]["date"] . ": " .$newEntries[$i]["hours"] . "\n";
+  
+        // $fullProjectId = $newEntries[$i]["organization"] . ":" . $newEntries[$i]["project"];
+        // if (!isset($projects[$fullProjectId])) {
+        //   $projects[$fullProjectId] = 0;
+        // }
+        // $projects[$fullProjectId] += $newEntries[$i]["hours"];
+      } else if ($newEntries[$i]["type"] == "contract") {
+        if (isset(($newEntries[$i]["from"]))) {
+          $fromWeek = dateTimeToWeekOfYear(($newEntries[$i]["from"]));
+        } else {
+          $fromWeek = ANALYSIS_FIRST_WEEK;
+        }
+        if (isset(($newEntries[$i]["to"]))) {
+          $toWeek = dateTimeToWeekOfYear(($newEntries[$i]["to"]));
+        } else {
+          $toWeek = ANALYSIS_LAST_WEEK;
+        }
+        if (isset(($newEntries[$i]["hours"]))) {
+          $hours = $newEntries[$i]["hours"];
+        } else {
+          $hours = DEFAULT_HOURS_PER_WEEK;
+        }
+        
+        $this->setHoursContracted($newEntries[$i]["organization"], $newEntries[$i]["worker"], $fromWeek, $toWeek, $hours);
+      }
+    }
+  }
+
+  function ensureDataStructure($organization, $worker, $week) {
+    if (!isset($this->structured[$organization])) {
+      $this->structured[$organization] = [];
+    }
+    if (!isset($this->structured[$organization][$worker])) {
+      $this->structured[$organization][$worker] = [];
+    }
+    if (!isset($this->structured[$organization][$worker][$week])) {
+      $this->structured[$organization][$worker][$week] = [
+        "hoursWorked" => 0,
+        "hoursContracted" => 0,
+        "details" => ""
+      ];
+    }
+  }
+  function weekExists($week) {
+    $numWeeks = [
+      "2020" => 53,
+      "2021" => 52,
+      "2022" => 53,
+      "2023" => 53,
+      "2024" => 53,
+      "2025" => 53,
+      "2026" => 53
+    ];
+    $year = substr($week, 0, 4);
+    $woy = substr($week, 4, 2);
+    if (intval($year) < 2020 || intval($year) > 2026) {
+      return false;
+    }
+    return (intval($woy) > 0 && intval($woy) <= $numWeeks[$year]);
+  }
+  
+  function setHoursContracted($organization, $worker, $fromWeek, $toWeek, $hours) {
+    for ($week = $fromWeek; $week <= $toWeek; $week++) {
+      if ($this->weekExists($week)) {
+        $this->ensureDataStructure($organization, $worker, $week);
+        $this->structured[$organization][$worker][$week]["hoursContracted"] = $hours;
+        debug("Organization $organization contracted  $worker for $hours hours in week of " . weekOfYearToDateTime($week) . "\n");
+      }
+    }
+  }
+  function checkHoursPerWeek() {
+    foreach($this->structured as $organization => $workers) {
+      foreach($workers as $worker => $weeks) {
+        sort($weeks);
+        foreach ($weeks as $week => $data) {
+          $hours = $data["hoursWorked"];
+          $contractHours = $data["hoursContracted"];
+          if ($hours != $contractHours) {
+            debug("In week $week (starting " . weekOfYearToDateTime($week) . "), $worker wrote $hours hours for $organization instead of $contractHours!\n", LEVEL_OUTPUT);
+            var_dump($data["details"]);
+          } else {
+            debug("In the week $week (starting " . weekOfYearToDateTime($week) . "), $worker wrote $hours hours for $organization which matches $contractHours!\n");
+          }
+        }
+      }
+    }
+  }
+  function loadSources($folderPath) {
+    // ignore the warning, we'll check for return value false below.
+    set_error_handler(function() { /* ignore errors */ });
+    $fileNames = scandir($folderPath);
+    restore_error_handler();
+    if ($fileNames == false) {
+      echo "Folder not found: '$folderPath'\n";
+      exit();
+    }
+  
+    for ($i = 0; $i < count($fileNames); $i++) {
+      if (($fileNames[$i] == ".") ||  ($fileNames[$i] == "..")) {
+        continue;
+      }
+      if (!str_ends_with($fileNames[$i], ".pj2")) {
+        echo "Filename '" . $fileNames[$i] . "' in '" . $folderPath . "' does not have a .pj2 extension .\n";
+        exit(1);
+      }
+      $contents = file_get_contents($folderPath . $fileNames[$i]);
+      try {
+        $pj2Entries =  json_decode($contents, true, 512, JSON_THROW_ON_ERROR);
+        // var_dump($pj2Entries);
+       debug("Contents of " . $folderPath . $fileNames[$i] . " parsed as " . count($pj2Entries) . " PJ2 flat.\n");
+  
+      } catch (Exception $e) {
+        echo "Contents of " . $folderPath . $fileNames[$i] . " is not JSON.\n";
+        exit(1);
+      }
+      $this->addEntries($pj2Entries);
+    }
+  }  
+}
