558e1ddc26560ca5c1bc140c1acabedfa155afa7
Michiel de Jong <michiel@pondersource.com>
2023-06-01 11:56:57 +0300

    Fix usage instructions

--- src/pj2-based/index.php
+++ src/pj2-based/index.php
@@ -3,7 +3,7 @@
 require_once(__DIR__ . '/Timesheet.php');
 
 if (count($_SERVER['argv']) < 3) {
-  echo "Not enough arguments. Usage e.g.:\nphp ./src/pj-based/index.php validate-working-hours ../../pondersource-books/stichting/source-docs/\n";
+  echo "Not enough arguments. Usage e.g.:\nphp ./src/pj2-based/index.php validate-working-hours ../../pondersource-books/stichting/build/\n";
   exit();
 }
 $cmd = $_SERVER['argv'][1];
