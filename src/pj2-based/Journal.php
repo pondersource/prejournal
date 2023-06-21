3d1fa76e6c1f26f4ead89db6e29355a75491663c
Michiel de Jong <michiel@pondersource.com>
2023-06-21 15:41:41 +0200

    OOP PJ2 to PTA

--- src/pj2-based/Journal.php
+++ src/pj2-based/Journal.php
@@ -12,8 +12,110 @@ function dateFromEntry($entry) {
     }
 }
 
+abstract class PrejournalEntry {
+  // private $type;
+
+  // private $worker;
+  // private $user;
+  // private $payer;
+
+  // private $date;
+  // private $from;
+  // private $to;
+  // private $paid;
+  // private $reimbursed;
+
+  // private $organization;
+  // private $project;
+  // private $supplier;
+
+  // private $amount;
+  // private $hours;
+  // private $description;
+
+  protected $fields;
+  function __construct($obj) {
+    $this->fields = $obj;
+  }
+
+  abstract function toJournalEntries();
+}
+
+class WorkedPrejournalEntry extends PrejournalEntry {
+  function toJournalEntries() {
+    return [new JournalEntry([
+      "date" => $this->fields["date"],
+      "account1" => $this->fields["worker"],
+      "account2" => $this->fields["project"],
+      "amount" => $this->fields["hours"],
+      "description" => "worked"
+    ])];
+  }
+}
+
+class SalaryPrejournalEntry extends PrejournalEntry {
+  function toJournalEntries() {
+    return [];
+  }
+}
+class ContractPrejournalEntry extends PrejournalEntry {
+  function toJournalEntries() {
+    return [];
+  }
+}
+class ExpensePrejournalEntry extends PrejournalEntry {
+  function toJournalEntries() {
+    return [];
+  }
+}
+class LoanPrejournalEntry extends PrejournalEntry {
+  function toJournalEntries() {
+    return [];
+  }
+}
+class IncomePrejournalEntry extends PrejournalEntry {
+  function toJournalEntries() {
+    return [];
+  }
+}
+
+function makePrejournalEntry($obj) {
+  switch ($obj["type"]) {
+    case "worked": return new WorkedPrejournalEntry($obj);
+    case "salary": return new SalaryPrejournalEntry($obj);
+    case "contract": return new ContractPrejournalEntry($obj);
+    case "expense": return new ExpensePrejournalEntry($obj);
+    case "loan": return new LoanPrejournalEntry($obj);
+    case "income": return new IncomePrejournalEntry($obj);
+    default: throw new Exception("Unknown PJ2 entry type $obj[type]");
+  }
+}
+
+class JournalEntry {
+  public $date;
+  public $account1;
+  public $account2;
+  public $amount;
+  public $description;
+  function __construct($obj) {
+    $this->date = $obj["date"];
+    $this->account1 = $obj["account1"];
+    $this->account2 = $obj["account2"];
+    $this->amount = $obj["amount"];
+    $this->description = $obj["description"];
+  }
+}
+
+function formatJournalEntry($entry) {
+   return "$entry->date $entry->description\n"
+    . "    $entry->account1  $entry->amount\n"
+    . "    $entry->account2\n";
+}
+
 function entryToPta($entry) {
-    return dateFromEntry($entry) . " " . $entry["type"] . "\n";
+  $JournalEntries = $entry->toJournalEntries();
+  $arr = array_map("formatJournalEntry", $JournalEntries);
+  return implode("\n\n", $arr);
 }
 
 function entrySortCb($a, $b) {
@@ -60,7 +162,8 @@ class Journal {
     usort($this->entries, 'entrySortCb');
     $highestDateYet = 0;    
     for ($i = 0; $i < count($this->entries); $i++) {
-        echo entryToPta($this->entries[$i]);
+        $obj = makePrejournalEntry($this->entries[$i]);
+        echo entryToPta($obj);
         $thisDate = new DateTime(dateFromEntry($this->entries[$i]));
         $thisStamp = $thisDate->getTimestamp();
         if ($thisStamp < $highestDateYet) {
